e39e6ce68c9efb94a0543e777197b0626f080553
Innocent Anoziee <iygeal@gmail.com>
2024-02-06 15:52:00 +0100

    Created the BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module that defines a class named BaseModel"""
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    """The Base Class of the module"""
+    def __init__(self):
+        """Method to initialize the class attributes"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Updates the updated_at attribute with current time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Method for serialization"""
+        serialized = self.__dict__
+        serialized['__class__'] = self.__class__.__name__
+        serialized.update({"created_at": self.created_at.isoformat(),
+                           "updated_at": self.updated_at.isoformat()})
+        return serialized
+
+    def __str__(self):
+        """Returns a visualization of the class objects"""
+        return "[{}] {} {}".format(self.__class__.__name__, self.id, self.__dict__)

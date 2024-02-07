79e57d429976d818ed17a548698ca6f6212bfe57
Innocent Anozie <iygeal@gmail.com>
2024-02-08 00:34:52 +0100

    Implemented a simple command interpreter using cmd module

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for the HBNB Console"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for HBNB"""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """This methods implement quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing when input is an empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+

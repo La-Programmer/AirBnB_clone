f87d42a143f42d8b80e68e69b488638366286b51
Innocent Anoziee <iygeal@gmail.com>
2024-02-10 11:28:35 +0100

    Modified FileStorage for task 8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,8 @@
 
 import os
 import json
+from models.user import User
+from models.base_model import BaseModel
 
 
 class FileStorage():
@@ -26,7 +28,7 @@ class FileStorage():
         """
         Sets obj in __objects with key <class name>.<obj.id>
         """
-        key = "{}.{}".format(obj.__class__.__name__.obj.id)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj.to_dict()
 
     def save(self):
@@ -36,7 +38,7 @@ class FileStorage():
         for key, obj in self.__objects.items():
             # Use the to_dict method to serialize each object
             serialized[key] = obj.to_dict()
-        
+
         data = json.dumps(serialized)
         with open(self.__file_path, 'w') as file:
             file.write(data)
@@ -46,5 +48,28 @@ class FileStorage():
         """
         if os.path.exists(self.__file_path):
             with open(self.__file_path, 'r') as file:
+                # Read the entire file and save to the variable, content
                 content = file.read()
-            self.__objects = json.loads(content)
+            # Check if the file content is empty
+            if content:
+                # Convert/load JSON file back to python obj (dict)
+                serialized = json.loads(content)
+
+                # Iterate through the serialized dict
+                for key, value in serialized.items():
+                    # Extract class name and object ID
+                    class_name, obj_id = key.split('.')
+
+                    # Check for the class name
+                    if class_name == 'BaseModel':
+                        # Create instance of the calss
+                        obj_instance = BaseModel(**value)
+                    elif class_name == 'User':
+                        obj_instance = User(**value)
+                    else:
+                        # Skip unknown classes
+                        continue
+
+                    # Set the object in __objects
+                    self.__objects[key] = obj_instance
+

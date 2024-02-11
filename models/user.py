d45438eddfcabef9221a1eb8abd372bd4fba77c8
Innocent Anozie <iygeal@gmail.com>
2024-02-11 15:43:01 +0100

    Made pycodestyle changes

--- models/user.py
+++ models/user.py
@@ -5,12 +5,14 @@
 from models.base_model import BaseModel
 from . import storage
 
+
 class User(BaseModel):
-	""" The User class
-	"""
-	def __init__(self, *args, **kwargs):
-		self.email = ''
-		self.password = ''
-		self.first_name = ''
-		self.last_name = ''
-		super().__init__(**kwargs)
+    """ The User class
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.email = ''
+        self.password = ''
+        self.first_name = ''
+        self.last_name = ''
+        super().__init__(**kwargs)
